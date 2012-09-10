3d768e84275d3e810241b50ebe391728bdc3874b
Udi Finkelstein <github@udif.com>
2012-09-11 02:11:50 +0300

    Added C model

---
+++ life.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define X 8
+#define Y 8
+#define ONE(x,y) (1LL << (X*(y)+x))
+#include <unistd.h>
+#include <termio.h>
+
+int getch( void )
+{
+	char ch;
+	int fd = fileno(stdin);
+	struct termio old_tty, new_tty;
+
+	ioctl(fd, TCGETA, &old_tty);
+	new_tty = old_tty;
+	new_tty.c_lflag &= ~(ICANON | ECHO | ISIG);
+	ioctl(fd, TCSETA, &new_tty);
+	fread(&ch, 1, sizeof(ch), stdin);
+	ioctl(fd, TCSETA, &old_tty);
+
+	return ch;
+}
+
+void main(int argc, char *argv[])
+{
+  unsigned long long data;
+  int x = 0;
+  int y = 0;
+  int gen;
+  int cell, new, sum, l , r, u, d, lu, ld, ru, rd, pipe, pipe_out;
+
+  data = ONE(1,2) | ONE(2,2) | ONE(3,2) | ONE(3,3) | ONE(2,4);
+
+  for (gen = 1; gen < 100; gen++) {
+    system("clear");
+    printf("Gen: %d\n", gen);
+    printf("---------------\n");
+    for (y = 0; y < Y; y++) {
+      for (x = 0; x < X; x++) {
+        cell = data & 1;
+        printf ("%01d ", cell);
+        l = (x < (X-1)) ? ((data >> 1) & 1)  : 0;
+        r = (x >= 1)    ? ((data >> (X*Y-1)) & 1) : 0;
+        u = (y < (Y-1)) ? ((data >> X) & 1)  : 0;
+        d = (y >= 1)    ? ((data >> ((Y-1)*X)) & 1) : 0;
+        lu = ((x < (X-1)) && (y < (Y-1))) ? ((data >> (X+1))  & 1)  : 0;
+        ru = ((x >= 1)    && (y < (Y-1))) ? ((data >> (X-1))  & 1)  : 0;
+        ld = ((x < (X-1)) && (y >= 1))    ? ((data >> ((Y-1)*X+1)) & 1)  : 0;
+        rd = ((x >= 1)    && (y >= 1))    ? ((data >> ((Y-1)*X-1)) & 1)  : 0;
+        sum = l + r + u + d + lu + ru + ld + rd;
+        new = (sum == 3) ? 1 : (sum == 2) ? cell : 0;
+        // printf ("x:%d y:%d new:%d sum:%d\n", x, y, new, sum);
+        pipe = (pipe << 1) | new;
+        pipe_out = ((pipe >> (X+2)) & 1);
+        data = (data >> 1) | ((data & 1) << (X*Y-1));
+        data = data & ~(1LL << ((Y-1)*X-3)) | (pipe_out  ? (1LL << ((Y-1)*X-3)) : 0);
+      }
+      printf("\n");
+    }
+    getch();
+  }
+}
+
