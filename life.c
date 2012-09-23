b8af811f02bc731f752984449a84016f7b9ecca5
Udi Finkelstein <github@udif.com>
2012-09-23 02:35:54 +0200

    No need for interactiveness

--- life.c
+++ life.c
@@ -1,26 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
 #define X 8
 #define Y 8
 #define ONE(x,y) (1LL << (X*(y)+x))
-#include <unistd.h>
-#include <termio.h>
-
-int getch( void )
-{
-	char ch;
-	int fd = fileno(stdin);
-	struct termio old_tty, new_tty;
-
-	ioctl(fd, TCGETA, &old_tty);
-	new_tty = old_tty;
-	new_tty.c_lflag &= ~(ICANON | ECHO | ISIG);
-	ioctl(fd, TCSETA, &new_tty);
-	fread(&ch, 1, sizeof(ch), stdin);
-	ioctl(fd, TCSETA, &old_tty);
-
-	return ch;
-}
 
 void main(int argc, char *argv[])
 {
