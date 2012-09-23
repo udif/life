26eabe0e7cbc21cbd8b39d26c97db7e48cfbf0f1
Udi Finkelstein <github@udif.com>
2012-09-24 01:28:21 +0200

    This is the correct file :-)

--- life_pipe.v
+++ life_pipe.v
@@ -5,7 +5,7 @@
 // 
 // Create Date:    21:14:54 09/09/2012 
 // Design Name: 
-// Module Name:    life_sum 
+// Module Name:    life_pipe
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,26 +18,25 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module life_sum #
+module life_pipe #
 (
 	parameter X=3'd8,
 	parameter Y=3'd8,
 	parameter LOG2X=3,
 	parameter LOG2Y=3
 ) (
-	output new_data,
-	input c, l, r, u, d, lu, ld, ru, rd
- );
+	input clk,
+	input new_data,
+	output pipe_out
+);
 
-wire [1:0]sum1, sum2, sum3;
-wire [2:0]total;
+reg [X:0]pipe;
 
-assign sum1 = {1'b0, lu} + {1'b0, u} + {ru, 1'b0};
-assign sum2 = {1'b0, l}  +             {r,  1'b0};
-assign sum3 = {1'b0, ld} + {1'b0, d} + {rd, 1'b0};
-// Notice we throw the 4th bit even though the sum can be 8 or 9
-assign total = {1'b0, sum1} + {1'b0, sum2} + {1'b0, sum3};
-// Notice that 2 or 3 must really be 2 or 3 and not 10 or 11 because we can't get those by summing 9 1's.
-assign new_data = (total == 3'd3) | (total == 3'd2) & c;
+always @(posedge clk)
+begin
+	pipe <= {pipe[X-1:0], new_data};
+end
+
+assign pipe_out = pipe[X];
 
 endmodule
