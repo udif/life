5802b8918f5994d57d1e05d3ef0e46d86879ebf5
Udi Finkelstein <github@udif.com>
2012-09-24 16:32:31 +0200

    fixed typing mistake (padded with 1'b0 on the right instead of the left).

--- life_sum.v
+++ life_sum.v
@@ -32,9 +32,9 @@ module life_sum #
 wire [1:0]sum1, sum2, sum3;
 wire [2:0]total;
 
-assign sum1 = {1'b0, lu} + {1'b0, u} + {ru, 1'b0};
-assign sum2 = {1'b0, l}  +             {r,  1'b0};
-assign sum3 = {1'b0, ld} + {1'b0, d} + {rd, 1'b0};
+assign sum1 = {1'b0, lu} + {1'b0, u} + {1'b0, ru};
+assign sum2 = {1'b0, l}  +             {1'b0, r};
+assign sum3 = {1'b0, ld} + {1'b0, d} + {1'b0, rd};
 // Notice we throw the 4th bit even though the sum can be 8 or 9
 assign total = {1'b0, sum1} + {1'b0, sum2} + {1'b0, sum3};
 // Notice that 2 or 3 must really be 2 or 3 and not 10 or 11 because we can't get those by summing 9 1's.
